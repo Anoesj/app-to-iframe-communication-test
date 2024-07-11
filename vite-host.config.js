d277916dc4cb8089a524579fc60d64395f44000f
Anoesj <anoesjsadraee@gmail.com>
2024-07-11 18:16:32 +0200

    Dynamic iframe src

--- vite-host.config.js
+++ vite-host.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-export default defineConfig({
-  root: './host',
-  server: {
-    port: 5173,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    root: './host',
+    server: {
+      port: 5173,
+    },
+    plugins: [
+      createHtmlPlugin({
+        inject: {
+          data: {
+            iframeSrc: env.VITE_IFRAME_SRC || 'http://localhost:5174',
+          },
+        },
+      }),
+    ],
+  };
 });
